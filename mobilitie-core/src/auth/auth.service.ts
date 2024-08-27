80fc2f2618c942fa9e4c2a59010ba516ee0ec5ad
Houselins <jcasalins@vortexbird.com>
2024-08-26 21:07:45 -0500

    feat: rol token and news model

--- mobilitie-core/src/auth/auth.service.ts
+++ mobilitie-core/src/auth/auth.service.ts
@@ -1,58 +1,60 @@
-import { Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
-import { UserDto } from './dto/user.dto'
+import {
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { UserDto } from './dto/user.dto';
 import { PrismaService } from 'src/shared';
-import { User } from '@prisma/client'
-import {JwtService} from '@nestjs/jwt'
+import { User } from '@prisma/client';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
 
-    constructor(
-      private prisma: PrismaService,
-      private jwtService: JwtService
-    ) { }
+  getUsers(): Promise<User[]> {
+    return this.prisma.user.findMany();
+  }
 
-    getUsers(): Promise<User[]> {
-        return this.prisma.user.findMany();
-    }
+  async login(usuario: User): Promise<{ access_token: string; user: any }> {
+    try {
+      const user = await this.prisma.user.findUnique({
+        where: { email: usuario.email },
+        include: { rol: true },
+      });
+      console.log('este es', user);
 
-    async login(usuario: User): Promise<{ access_token: string, user: any }> {
-      try {
-        
-        const user = await this.prisma.user.findUnique({
-          where: { email: usuario.email },
-        });
-        console.log('este es',user);
-        
-  
-        if (!user || usuario.password !== user.password) {
-          throw new UnauthorizedException('Invalid credentials');
-        }
-  
-        await this.prisma.loginHistory.create({
-          data: {
-            userId: user.id,
-            loginTime: new Date(),
-          },
-        });
-  
-        // Generar un token JWT
-        const payload = { username: user.email, id: user.id };
-        const access_token = this.jwtService.sign(payload);
-  
-        return {
-          access_token,
-          user: {
-            name: user.name, 
-            email: user.email
-          },
-        };
-      } catch (error) {
-        if (error instanceof UnauthorizedException) {
-          throw error;
-        }
-        throw new InternalServerErrorException('An error occurred during login');
+      if (!user || usuario.password !== user.password) {
+        throw new UnauthorizedException('Invalid credentials');
       }
+
+      await this.prisma.loginHistory.create({
+        data: {
+          userId: user.id,
+          loginTime: new Date(),
+        },
+      });
+
+      // Generar un token JWT
+      const payload = { username: user.email, id: user.id };
+      const access_token = this.jwtService.sign(payload);
+
+      return {
+        access_token,
+        user: {
+          name: user.name,
+          email: user.email,
+          rol: user.rol,
+        },
+      };
+    } catch (error) {
+      if (error instanceof UnauthorizedException) {
+        throw error;
+      }
+      throw new InternalServerErrorException('An error occurred during login');
     }
-      
+  }
 }
